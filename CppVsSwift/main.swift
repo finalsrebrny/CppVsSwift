cd2760af9f37ff023894551a87d949799977c945
Piotr Holub <221012@student.pwr.edu.pl>
2018-08-02 21:22:00 +0200

    Initial Commit

---
+++ CppVsSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CppVsSwift
+//
+//  Created by Piotr Holub on 02.08.2018.
+//  Copyright © 2018 Piotr Holub. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
