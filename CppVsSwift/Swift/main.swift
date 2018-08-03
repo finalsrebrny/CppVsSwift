413e748626afa51f02d607efb54bc1f502a483cd
Unknown <221012@student.pwr.edu.pl>
2018-08-03 22:51:00 +0200

    Auto stash before merge of "master" and "github/master"

--- CppVsSwift/Swift/main.swift
+++ CppVsSwift/Swift/main.swift
@@ -9,39 +9,5 @@
 import Foundation
 import QuartzCore
 
-print("Hello, World!")
-
-let cpp = HelloFromCPP()
-
-cpp.hello(fromCPP: "Piotr")
-
-let cpp2 = QuickSortCpp()
-
-print()
-print("Cpp quicksort vs Swift sort metod")
-
-
-
-
-
-
-
-func makeList(_ n: Int) -> [Int] {
-    return (0..<n).map{ _ in Int(arc4random_uniform(2000000) + 1) }
-}
-
-
-var list = makeList(1000000)
-var start = CACurrentMediaTime()
-list.sort()
-var end = CACurrentMediaTime()
-print("swift:\(end-start)")
-
-let cpp3 = AlgorithmApi()
-
-var list2 = makeList(1000000)
-var start2 = CACurrentMediaTime()
-cpp3.sortIntegers(list2)
-var end2 = CACurrentMediaTime()
-print("cpp :\(end2-start2)")
+SortReport()
 
